f00b2a053269ee60579b2d1e415733bd2ddb7944
Nikolay Stanchev <nstanchev@vmware.com>
2019-06-12 14:01:41 +0300

    Add structs, refactor main.go
    Signed-off-by: Nikolay Stanchev <nstanchev@vmware.com>

--- slices.go
+++ slices.go
@@ -1,7 +1,20 @@
 package main
 
-func slices() []int {
+import (
+	"fmt"
+)
+
+func ShowcaseSlices() {
+
+	var numbers [5]int
+	// how to define array explicitly
+	var numbers2 = []float32{1, 2, 3, 4, 21}
+	// this is a slice, dynamic array
+	var numbersD []int
+
+	numbersD = make([]int, 5, 5)
+
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	var s []int = primes[1:4]
-	return s
+	fmt.Println(numbers, numbers2, numbersD, s)
 }
--- structs.go
+++ structs.go
@@ -1,6 +1,61 @@
 package main
 
+import "fmt"
+
 type Vertex struct {
-	x int
-	y int
+	x    int
+	y    int
+	name string
+}
+
+type Salary struct {
+	Basic, HRA, TA float64
+}
+
+type Employee struct {
+	FirstName, LastName, Email string
+	Age                        int
+	MonthlySalary              []Salary
+}
+
+func ShowcaseStructs() {
+	v := Vertex{1, 2, "example1"} // can't skip values
+	var v1 Vertex
+	v1.x = 2
+	v1.y = 3
+	var v2 = Vertex{x: 1, y: 2} // name skipped
+	var v3 = new(Vertex)        // v3 is a pointer to an instance of Vertex
+	fmt.Println(v, v1, v2, v3)
+	// Nested structs
+
+	e := Employee{
+		FirstName: "Mark",
+		LastName:  "Jones",
+		Email:     "mark@gmail.com",
+		Age:       25,
+		MonthlySalary: []Salary{
+			Salary{
+				Basic: 15000.00,
+				HRA:   5000.00,
+				TA:    2000.00,
+			},
+			Salary{
+				Basic: 16000.00,
+				HRA:   5000.00,
+				TA:    2100.00,
+			},
+			Salary{
+				Basic: 17000.00,
+				HRA:   5000.00,
+				TA:    2200.00,
+			},
+		},
+	}
+	fmt.Println(e.FirstName, e.LastName)
+	fmt.Println(e.Age)
+	fmt.Println(e.Email)
+	fmt.Println(e.MonthlySalary[0])
+	fmt.Println(e.MonthlySalary[1])
+	fmt.Println(e.MonthlySalary[2])
+
 }
